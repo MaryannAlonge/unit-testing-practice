b214a2690c9c31535096acbcc93d58229eb8df36
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-17 01:41:49 +0100

    more tests

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -1,6 +1,8 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import Todo from "../todo"
 
+// use the cleanup method to cleanup after each text
+
 test("should render non-completed todo ", () => {
   const todo = {id: 1, title: "wash dishes", completed: false, }
   // use the render function imported above and pass in todo prop
@@ -10,5 +12,17 @@ test("should render non-completed todo ", () => {
   // make assertions
   expect(todoElement).toBeInTheDocument();
   expect(todoElement).toHaveTextContent("wash dishes");
+
+});
+
+test("should render completed todo ", () => {
+  const todo = {id: 2, title: "wash car", completed: true, }
+  // use the render function imported above and pass in todo prop
+  render(<Todo todo={todo}/>);
+  // retrieve the component
+  const todoElement = screen.getByTestId("todo-2");
+  // make assertions
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("wash car");
 
 });
