fd63899c8b44c6a2fe26b9c70c9fc137812a9d0d
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-17 01:08:22 +0100

    import test libraries

---
+++ src/components/__tests__/todo.test.js
@@ -0,0 +1,9 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Todo from "../todo"
+
+test("should render todo component", () => {
+  // use the render function imported above
+  render(<Todo/>);
+  // retrieve the component
+  const todoElement = screen.getAllByTestId();
+})
